6c63db3023e9dc1f05399ced277862d5fe238b00
Dusan Perkovic <dusan.perkovic@soprex.com>
2017-10-17 10:10:44 +0200

    Fetching all questions from the backend.

--- src/app/question.service.ts
+++ src/app/question.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { DropdownQuestion } from './dynamic-form/question-dropdown';
-import { QuestionBase } from './dynamic-form/question-base';
 import { TextboxQuestion } from './dynamic-form/question-textbox';
 import 'rxjs/add/operator/map';
 
@@ -14,41 +13,14 @@ export class QuestionService {
 
         return this.http
             .get<any[]>('http://localhost:3000/questions')
-            .map(result => result.map(question => new TextboxQuestion(question)));
-            // .map(result => {
-            //     return Array(result).map(question => new TextboxQuestion(question));
-            // });
-
-        // const questions: QuestionBase<any>[] = [
-        //
-        //     new DropdownQuestion({
-        //         key: 'brave',
-        //         label: 'Bravery Rating',
-        //         options: [
-        //             {key: 'solid', value: 'Solid'},
-        //             {key: 'great', value: 'Great'},
-        //             {key: 'good', value: 'Good'},
-        //             {key: 'unproven', value: 'Unproven'},
-        //         ],
-        //         order: 3,
-        //     }),
-        //
-        //     new TextboxQuestion({
-        //         key: 'firstName',
-        //         label: 'First Name',
-        //         value: 'Bombasto',
-        //         required: true,
-        //         order: 1
-        //     }),
-        //
-        //     new TextboxQuestion({
-        //         key: 'emailAddress',
-        //         label: 'Email',
-        //         type: 'email',
-        //         order: 2
-        //     })
-        // ];
-        //
-        // return questions.sort((a, b) => a.order - b.order);
+            .map(result => result.map(question => {
+                switch (question.controlType) {
+                    case 'text':
+                        return new TextboxQuestion(question);
+                    case 'dropdown':
+                        return new DropdownQuestion(question);
+                }
+            }))
+            .map(questionArray => questionArray.sort((a, b) => a.order - b.order));
     }
 }
